bbff79343550366c695fe1f9abf117338e90dae8
tobychadderton1 <hapaxemail@mail.com>
2020-05-19 22:16:28 +0100

    Implemented better code practice

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ num = int(numfile.read())
 maxnum = 10
 
 def DeepFry(image):
-    size = width , height = image.size
+    size = image.size
     sharp = ImageEnhance.Sharpness (image.convert('RGB'))
     image = sharp.enhance(5.0)
     colour = ImageEnhance.Color (image)
@@ -15,24 +15,27 @@ def DeepFry(image):
     return image
 
 def AddText(image):
-    width, height = image.size
-    font =ImageFont.truetype("impact.ttf",(int(width/4)))
+    width = image.size[0]
+    height = image.size[1]
+    
+    font = ImageFont.truetype("impact.ttf",(int(width/4)))
     wordlist = open("wordlist.txt").read().splitlines()
     rndword = random.choice(wordlist)
 
     for char in rndword:
-        num=+1
+        num = 1
 
     if num > 7:
-        font =ImageFont.truetype("impact.ttf",(int(width/4)))
-    if num <7:
-        font =ImageFont.truetype("impact.ttf",(int(width/8)))
+        font = ImageFont.truetype("impact.ttf", (int(width/4)))
+    if num < 7:
+        font = ImageFont.truetype("impact.ttf", (int(width/8)))
     
-    tw,th=font.getsize(rndword)
+    tw = font.getsize(rndword)[0]
+    th = font.getsize(rndword)[1]
     draw = ImageDraw.Draw(image)
-    uw =int((width-tw)/2)
-    uh =int((height-th)/2)
-    draw.text((uw,10),rndword,font=font)
+    uw = int((width-tw)/2)
+    uh = int((height-th)/2)
+    draw.text((uw, 10), rndword, font=font)
     return image
 
 
@@ -41,8 +44,8 @@ maxnum = int(input("How Many?: "))
 for i in range(maxnum):
     randomfile = random.choice(os.listdir("imageLibrary"))
     randomimg = randomfile
-    num +=1
-    AddText(DeepFry(Image.open(os.path.join("imageLibrary" , randomimg)))).save("finalimage"+str(num)+".png")
+    num += 1
+    AddText(DeepFry(Image.open(os.path.join("imageLibrary", randomimg)))).save("finalimage"+str(num)+".png")
 
 numfile.seek(0)
 numfile.write(str(num))
